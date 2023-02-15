ee1d871f7b88e1f92c2898bb58b3f9648d6cc4ac
davidmelgares <davidmelgareslazaro@gmail.com>
2023-02-15 18:00:39 +0100

    First commit

---
+++ app/src/main/java/com/example/getthepic/gtidic/udl/getthepic/getthepic/jjd2223/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.getthepic.gtidic.udl.getthepic.getthepic.jjd2223;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
