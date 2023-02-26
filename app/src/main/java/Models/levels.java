5e968d1f6bea7cebda440dda2d07638f720b4e26
joanjaume <fotosjoanj@gmail.com>
2023-02-26 19:55:14 +0100

    Splash Screen Logo LuxeSoft

---
+++ app/src/main/java/Models/levels.java
@@ -0,0 +1,12 @@
+package Models;
+
+public class levels {
+
+    public static String[] levels = {"COTXE","CAMISA","ESCOLA"};
+
+
+    public static String GetLevel(int level)
+    {
+        return levels[level];
+    }
+}
--- app/src/main/java/com/example/getthepic/gtidic/udl/getthepic/getthepic/jjd2223/GlobalInfo.java
+++ app/src/main/java/com/example/getthepic/gtidic/udl/getthepic/getthepic/jjd2223/GlobalInfo.java
@@ -1,5 +1,12 @@
 package com.example.getthepic.gtidic.udl.getthepic.getthepic.jjd2223;
 
 public class GlobalInfo {
-    public static final int SPLASH_SCREEN_TIMEOUT = 2000;
+    public static final int SPLASH_SCREEN_TIMEOUT = 4000;
+
+    public static int LAST_LEVEL = 0;
+
+    public void UpdateLastLevel(int n)
+    {
+        LAST_LEVEL = n;
+    }
 }
