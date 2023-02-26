f14356b3b3d33a0e68e6713da0236b398b99e068
joanjaume <fotosjoanj@gmail.com>
2023-02-26 20:12:17 +0100

    Mecànica del joc en progres TOT

--- app/src/main/java/Models/Game.java
+++ app/src/main/java/Models/Game.java
@@ -1,9 +1,19 @@
 package Models;
 
+import com.example.getthepic.gtidic.udl.getthepic.getthepic.jjd2223.GlobalInfo;
+
 import Models.Player.Player;
 
 public class Game {
-    int maxPoints = -1;
+    int LEVEL = GlobalInfo.LAST_LEVEL;
     Player currentPlayer;
     Player winner;
-}
+    Board board;
+
+    public void init()
+    {
+        int boardSize = 8;
+
+        board = new Board(4);
+        board.load(LEVEL);
+    }
--- app/src/main/java/Models/Piece.java
+++ app/src/main/java/Models/Piece.java
@@ -1,5 +1,13 @@
 package Models;
 
 public class Piece {
-    String value;
+    private char value;
+    public void setValue(char lletra)
+    {
+        value = lletra;
+    }
+    public char getValue()
+    {
+        return value;
+    }
 }
